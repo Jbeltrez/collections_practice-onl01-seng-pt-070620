cbe0de7fe9483024a17d4c8f0abd7bb0ec4b52f9
john beltrez <beltrezjohn@gmail.com>
2020-07-18 05:18:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,10 +1,7 @@
 def sort-array_asc(array)
-  # Question 1
-  describe '#sort_array_asc' do
-    it 'should return an array sorted in ascending order' do
-      expect(sort_array_asc([25, 7, 1])).to eq([1,7,25])
-    end
-  end
+  array.sort do |a, b|
+  a <=> b
+ end 
 end 
 
 def sort_array_desc 
