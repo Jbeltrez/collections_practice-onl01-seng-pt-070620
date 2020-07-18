77f616fdf7421abf536864c56316441ab8688979
john beltrez <beltrezjohn@gmail.com>
2020-07-18 05:15:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,10 +44,16 @@ def reverse_array
 end 
 
 def kesha_maker 
-  
+  # Question 6
+  describe '#kesha_maker' do
+    # DO NOT USE .collect or .map. use .each to build a new array, like in the "my_each" lab!
+    it 'taking an array as an input, change the 3rd character of each element to a dollar sign.' do
+      expect(kesha_maker(["blake", "ashley", "scott"])).to eq(["bl$ke", "as$ley", "sc$tt"])
+    end
 end 
 
 def find_a
+  
 end 
 
 def sum_array 
