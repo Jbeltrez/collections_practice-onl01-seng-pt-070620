8a5df6e9f45dc5b99df03e67ffe09abcb210aeef
john beltrez <beltrezjohn@gmail.com>
2020-07-18 05:58:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,17 +23,7 @@ def reverse_array(array)
 end 
 
 def kesha_maker(array)
-  array.each do {|item| ounter = 0 
-  while counter < array.length 
-  yield (array[counter])
-  
-  counter += 1
-  end
+  array.each {|item| intem[2] = $}
   array
-  # Question 6
-  describe '#kesha_maker' do
-    # DO NOT USE .collect or .map. use .each to build a new array, like in the "my_each" lab!
-    it 'taking an array as an input, change the 3rd character of each element to a dollar sign.' do
-      expect(kesha_maker(["blake", "ashley", "scott"])).to eq(["bl$ke", "as$ley", "sc$tt"])
-    end
-end 
+end
+  
