f0881e35697170dd0e19c2d8a8b53092fe4aba99
john beltrez <beltrezjohn@gmail.com>
2020-07-18 05:41:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,7 +9,7 @@ def sort_array_desc(array)
 end 
 
 def sort_array_char_count(array)
-  array.sort do {|left, right| 
+  array.sort do {|a, b| 
   a <=> b # should return an array in ascending order sorted by the number of characters in the string
  end
 end 
