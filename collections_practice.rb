74197bd4fbaa926af168b53aef682265564608e6
john beltrez <beltrezjohn@gmail.com>
2020-07-18 05:30:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,13 +9,6 @@ def sort_array_desc
 end 
 
 
-# Question 2
-  describe '#sort_array_desc' do
-    it 'should return an array sorted in descending order' do
-      expect(sort_array_desc([25, 7, 14])).to eq([25, 14, 7])
-    end
-  end
-end 
 
 def sort_array_char_count
   # Question 3
